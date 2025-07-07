b8b5832f45053f3814674ff8a57b9c62502da413
jatinKumar001443 <mrbansal157134@gmail.com>
2025-07-07 20:06:30 +0530

    Delete models_Expense.js

--- models_Expense.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ExpenseSchema = new mongoose.Schema({
-  name: String,
-  amount: Number,
-  date: Date
-});
-
-module.exports = mongoose.model('Expense', ExpenseSchema);
